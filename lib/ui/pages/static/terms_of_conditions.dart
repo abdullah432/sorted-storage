63e4c68b8d93c050e3d25ba82aeb340856453f5d
Jsuppers <jorissuppers@gmail.com>
2020-12-04 03:10:22 +0200

    created terms and privacy files

--- lib/ui/pages/static/terms_of_conditions.dart
+++ lib/ui/pages/static/terms_of_conditions.dart
@@ -5,27 +5,34 @@ class TermsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: RichText(
-        text: TextSpan(
-          style: DefaultTextStyle.of(context).style,
-          children: <TextSpan>[
-            TextSpan(
-              text: "",
-              style: TextStyle(color: Colors.black.withOpacity(0.6)),
-            ),
-            TextSpan(
-              text: "You don't have the votes!\n",
-              style: TextStyle(color: Colors.black.withOpacity(0.8)),
-            ),
-            TextSpan(
-              text: "You're gonna need congressional approval and you don't have the votes!\n",
-              style: TextStyle(color: Colors.black.withOpacity(1.0)),
-            ),
-          ],
-        ),
-      )
-    );
+    TextStyle normal = TextStyle(
+        color: Colors.black.withOpacity(0.6),
+        fontSize: 12,
+        fontWeight: FontWeight.normal);
+    return FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('docs/terms.txt'),
+        builder: (context, document) {
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Card(
+                child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: document.data,
+                      style: TextStyle(
+                          color: Colors.black.withOpacity(0.6),
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ],
+                ),
+              ),
+            )),
+          );
+        });
   }
-
 }
